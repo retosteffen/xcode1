26508168ad69109ce36a9101b9b7ae7367070c02
Reto Steffen <retosteffen@mac.com>
2012-06-29 10:50:26 +0200

    start of extra credit

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -68,6 +68,20 @@
         [self digitPressed:sender];
     }
 }
+- (IBAction)backspacePressed {
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        NSUInteger length=self.display.text.length;
+        if ((int)length-1<0){}
+        else if ((int) length-1==0) {
+            self.display.text=[self.display.text substringToIndex:length-1];
+            self.display.text=@"0";
+            self.userIsInTheMiddleOfEnteringANumber=NO;
+        }
+        else {
+            self.display.text=[self.display.text substringToIndex:length-1];
+        }
+    }
+}
 
 
 - (IBAction)clearAllPressed {
